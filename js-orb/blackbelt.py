d3695d5b12935ba8fa5c1a2322f5ad8a315e0bfe
ShovelCode <vde435@gmail.com>
2020-12-25 22:32:10 -0600

    update

---
+++ js-orb/blackbelt.py
@@ -0,0 +1,25 @@
+alabama = ["Bullock", "Dallas", "Greene", "Hale", "Lowndes", "Macon", "Marengo", "Montgomery", "Perry", "Sumter", "Wilcox"];
+
+arkansas = ["Chicot","Crittenden", "Jefferson", "Lee", "Phillips", "St. Francis"];
+
+florida = ["Gadsden"];
+
+georgia = ["Baker", "Burke", "Calhoun", "Clay", "Clayton", "DeKalb", "Dougherty", "Hancock", "Jefferson", "Macon", "Randolph", "Stewart", "Talbot", "Taliaferro", "Terrell", "Warren", "Washington"];
+
+louisiana = ["East Carroll", "Madison", "Orleans", "St. Helena", "St. James", "St. John the Baptist", "Tensas", "West Feliciana"];
+
+maryland = ["Baltimore City", "Prince George's"];
+
+mississippi = ["Adams", "Bolivar", "Claiborne", "Clay", "Coahoma", "Copiah", "Hinds", "Holmes", "Humphreys", "Issaquena", "Jasper", "Jefferson", "Jefferson Davis", "Kemper", "Leflore", "Marshall", "Noxubee", "Quitman", "Sharkey", "Sunflower", "Tallahatchie", "Tunica", "Washington", "Wilkinson", "Yazoo"];
+
+missouri = ["St. Louis City"];
+
+northcarolina = ["Anson", "Bertie", "Edgecombe", "Halifax", "Hertford", "Northampton", "Warren"];
+
+southcarolina = ["Allendale", "Bamberg", "Clarendon", "Fairfield", "Hampton", "Jasper", "Lee", "McCormick", "Marion", "Marlboro", "Orangeburg", "Williamsburg"];
+
+tennessee = ["Haywood", "Shelby"];
+
+virginia = ["Brunswick", "Charles City", "Emporia City", "Franklin City", "Greensville", "Petersburg City", "Portsmouth City", "Richmond City", "Surry", "Sussex"];
+
+#end
